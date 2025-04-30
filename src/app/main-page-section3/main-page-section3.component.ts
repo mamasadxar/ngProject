ec64d2ffb69aa572757fcdacd5374a05e3e96963
mamasadxar <magalavano34@gmail.com>
2025-05-01 02:46:51 +0400

    jack shit of nothing

--- src/app/main-page-section3/main-page-section3.component.ts
+++ src/app/main-page-section3/main-page-section3.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-page-section3',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink],
   templateUrl: './main-page-section3.component.html',
-  styleUrl: './main-page-section3.component.scss'
+  styleUrl: './main-page-section3.component.scss',
 })
 export class MainPageSection3Component {
 
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -3,10 +3,11 @@ import { MainPageHeaderComponent } from '../main-page-header/main-page-header.co
 import { MainPageSection1Component } from '../main-page-section1/main-page-section1.component';
 import { MainPageSection2Component } from '../main-page-section2/main-page-section2.component';
 import { MainPageSection3Component } from '../main-page-section3/main-page-section3.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
-  imports: [MainPageHeaderComponent, MainPageSection1Component, MainPageSection2Component, MainPageSection3Component],
+  imports: [MainPageHeaderComponent, MainPageSection1Component, MainPageSection2Component, MainPageSection3Component, RouterOutlet],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
