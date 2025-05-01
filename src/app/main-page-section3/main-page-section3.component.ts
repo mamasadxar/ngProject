32aa6cdacabf0a1213149893438be15562378e35
mamasadxar <magalavano34@gmail.com>
2025-05-02 02:41:11 +0400

    fixed

--- src/app/main-page-section3/main-page-section3.component.ts
+++ src/app/main-page-section3/main-page-section3.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-page-section3',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './main-page-section3.component.html',
   styleUrl: './main-page-section3.component.scss',
 })
