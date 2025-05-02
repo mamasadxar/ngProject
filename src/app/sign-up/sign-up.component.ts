efd7f7f8aa22e7f4d24086a092ac88d286cf17e8
mamasadxar <magalavano34@gmail.com>
2025-05-03 03:31:07 +0400

    ragaca ari

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+	registrationForm = new FormGroup({
+		username: new FormControl(''),
+		email: new FormControl(''),
+		password: new FormControl(''),
+	})
 
+	onSubmit() {
+		
+	}
 }
