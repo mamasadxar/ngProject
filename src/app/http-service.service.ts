675194000cc0d5f12337db92002e190b684be1e6
mamasadxar <magalavano34@gmail.com>
2025-05-15 23:37:10 +0400

    ARSAIDAN SHARSHI VICHITEBI

--- src/app/http-service.service.ts
+++ src/app/http-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product, ProductResponse } from './shop-page-items/product.model';
 
@@ -9,10 +9,14 @@ import { Product, ProductResponse } from './shop-page-items/product.model';
 export class HttpServiceService {
   public productsUrl = 'https://api.everrest.educata.dev/shop/products';
     
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private hcm: HttpClientModule) { }
   
   getAllProducts():Observable<Product[]> {
     return this.http.get<Product[]>(`https://api.everrest.educata.dev/shop/products/all?page_index=1&page_size=10`);
+  }
+   
+  postData(data: any): Observable<any> {
+    return this.http.post("https://api.everrest.educata.dev/auth/sign_up", data);
   }
 }
 
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -2,18 +2,41 @@ import { Component } from '@angular/core';
 import { FormGroup, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { MainPageHeaderComponent } from "../main-page-header/main-page-header.component";
 import { ShopPageHeaderComponent } from "../shop-page-header/shop-page-header.component";
+import { HttpClientModule } from '@angular/common/http';
+import { HttpServiceService } from '../http-service.service';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [ReactiveFormsModule, MainPageHeaderComponent, ShopPageHeaderComponent],
+  imports: [ReactiveFormsModule, ShopPageHeaderComponent, HttpClientModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+	constructor(private http: HttpServiceService) {}
+
 	registrationForm = new FormGroup({
-		username: new FormControl(''),
+		firstName: new FormControl(''),
+		lastName: new FormControl(''),
+		age: new FormControl(''),
 		email: new FormControl(''),
 		password: new FormControl(''),
+		address: new FormControl(''),
+		phone: new FormControl(''),
+		zipcode: new FormControl(''),
+		avatar: new FormControl(''),
+		gender: new FormControl(''),
 	})
 
+	onSubmit() {
+		let formValue = this.registrationForm.value;
+
+		this.http.postData(formValue).subscribe(
+			(response) => {
+				console.log('gaigzavnaAMGELOOOO', response);
+			},
+			(error) => {
+				console.log('azzearxar', error)
+			}	
+		)
+	}
 }
